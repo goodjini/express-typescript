9db54acb7277a0c3382d2d03afc7eae688f23c9f
Your Name <you@example.com>
2024-04-01 16:12:39 +0900

    embedding log4js

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,24 @@ import cors, { CorsOptions } from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 import { router } from './router';
+import log4js from 'log4js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const logConfigure: log4js.Configuration = {
+  "appenders": {
+    "console": { "type": "console" },
+    "file": { "type": "file", "filename": "logs/server.log" }
+  },
+  "categories": {
+    "default": { "appenders": ["console", "file"], "level": "debug" }
+  }
+} 
+
+const logger = log4js.configure(logConfigure).getLogger();
 
 const corsOptions: CorsOptions = {
   origin: ['http://localhost:3000'],
@@ -24,4 +37,5 @@ app.use('/users',router);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
+  logger.log(`Server is running on port: ${port}`);
 });
